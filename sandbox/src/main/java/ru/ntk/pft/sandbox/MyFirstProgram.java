c973351e88f52e8ec78d12f1f9da11051909f229
Chekholdinav <cheholdin_alex@mail.ru>
2018-02-05 18:45:21 +0700

    Изменены настройки форматирования

--- sandbox/src/main/java/ru/ntk/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/ntk/pft/sandbox/MyFirstProgram.java
@@ -1,10 +1,8 @@
 package ru.ntk.pft.sandbox;
 
 public class MyFirstProgram {
-	public static void main (String[] args){
-		
+    public static void main(String[] args) {
+        System.out.println("Hello world");
 
-System.out.println("Hello world");
-		
-	}
+    }
 }
