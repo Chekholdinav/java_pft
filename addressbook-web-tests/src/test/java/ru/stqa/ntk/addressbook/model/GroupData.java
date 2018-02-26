eb194535617c46eff5c4a5e60dfc5d4acf12d7be
Chekholdinav <cheholdin_alex@mail.ru>
2018-02-26 23:36:48 +0700

    Заменил наследование на делегирование

--- addressbook-web-tests/src/test/java/ru/stqa/ntk/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ntk/addressbook/model/GroupData.java
@@ -1,4 +1,4 @@
-package ru.stqa.ntk.addressbook;
+package ru.stqa.ntk.addressbook.model;
 
 public class GroupData {
   private final String name;
---
+++ addressbook-web-tests/src/test/java/ru/stqa/ntk/addressbook/tests/GroupCreationTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.ntk.addressbook.tests;
+
+
+import org.testng.annotations.Test;
+import ru.stqa.ntk.addressbook.model.GroupData;
+
+public class GroupCreationTests extends TestBase {
+
+  @Test
+  public void testGroupCreation() {
+    app.gotoGroupPage();
+    app.initGroupCreation();
+    app.fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.submitGroupCreation();
+    app.returnToGroupPage();
+  }
+
+}
