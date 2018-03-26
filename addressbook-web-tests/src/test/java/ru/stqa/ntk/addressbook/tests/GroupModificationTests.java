27ca44a6423a789fda3b8fe264f00fda7927c2c5
Chekholdinav <cheholdin_alex@mail.ru>
2018-03-26 22:51:48 +0700

    Реализована проверка и обеспечение выполнения предусловий для групп

--- addressbook-web-tests/src/test/java/ru/stqa/ntk/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ntk/addressbook/tests/GroupModificationTests.java
@@ -3,10 +3,13 @@ package ru.stqa.ntk.addressbook.tests;
 import org.testng.annotations.Test;
 import ru.stqa.ntk.addressbook.model.GroupData;
 
-public class GroupModificationTests extends TestBase{
+public class GroupModificationTests extends TestBase {
   @Test
-  public void groupModificationTests (){
+  public void groupModificationTests() {
     app.getNavigationHelper().gotoGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, "test3"));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test4"));
